772d641638cdcd1c14a6e9e6e004428f97a63d89
brayn <voidpointers@hotmail.com>
2020-03-07 02:19:20 +0800

    debug

--- app/Product/Entities/Listing.php
+++ app/Product/Entities/Listing.php
@@ -84,8 +84,8 @@ class Listing extends Model
                 $data['image_id'] = $images[0]['listing_image_id'];
                 $data['image'] = $images[0]['url_fullxfull'];
             }
-            if (array_key_exists('tags', $param)) {
-                $data['tags'] = json_encode($param['tags']);
+            if ('tags' == $key) {
+                $data['tags'] = json_encode($param);
             }
         }
 
